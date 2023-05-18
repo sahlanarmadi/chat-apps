92f2fda416e401a08888bdb472313c71b83aca77
sahlanarmadi <sahlanarmadi2303@gmail.com>
2023-05-19 03:27:32 +0700

    first commit

---
+++ chatapps.js
@@ -0,0 +1,39 @@
+const app = require ("express")()
+const http = require("http")
+const server = http.createServer(app)
+const {Server} = require ("socket.io")
+const io = new Server(server)
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/chatapps.html');
+  });
+  
+  let user = 1
+  let id = 1
+  io.on('connection', (socket) => {
+    socket.on("message", (data) => {
+      console.log("data =>", data)
+      const {id, message} = data
+      socket.broadcast.emit("message", id, message)
+    })
+    socket.on("join", (socket) => {
+      console.log("user login")
+      user++;
+      io.emit("user", user)
+    })
+    socket.on("id", (socket) => {
+      console.log(id++)
+      id++;
+      io.emit("id", id)
+    })
+    socket.on("disconnect", (socket) => {
+      console.log("user keluar")
+      user--;
+      io.emit("user", user)
+    })
+    });
+  
+  server.listen(3000, () => {
+    console.log(`http://localhost:${3000}`);
+  });
